6c0f2c44a99673eec2cb8fb856b2040531accde1
Eren Aral <araleren03@gmail.com>
2026-02-28 03:26:15 +0300

    🚀 Initial Commit: The Pact - Dual Architecture (React Native & Spring Boot)

---
+++ pact-backend/src/test/java/com/eren/pact_backend/PactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eren.pact_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
